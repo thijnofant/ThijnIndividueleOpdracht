979f1a55f61848c7b4382610bda0e31f359dc6fc
Thijn van Dijk <tavandijk@live.nl>
2014-05-22 10:52:23 +0200

    added empty database class

---
+++ ThijnVanDijk_IndividueleOpdrach_SE22/ThijnVanDijk_IndividueleOpdrach_SE22/Classes/DBConnect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThijnVanDijk_IndividueleOpdrach_SE22
+{
+    public class DBConnect
+    {
+        throw new NotImplementedException();
+    }
+}
