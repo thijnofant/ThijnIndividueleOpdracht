da25d8e61bbfcd375c420d63c6d90383a8c57d82
Thijn van Dijk <tavandijk@live.nl>
2014-05-23 15:54:21 +0200

    fixed the database class v1

--- ThijnVanDijk_IndividueleOpdrach_SE22/ThijnVanDijk_IndividueleOpdrach_SE22/Classes/DBConnect.cs
+++ ThijnVanDijk_IndividueleOpdrach_SE22/ThijnVanDijk_IndividueleOpdrach_SE22/Classes/DBConnect.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Oracle.DataAccess.Client;
 
 namespace ThijnVanDijk_IndividueleOpdrach_SE22
 {
     public class DBConnect
-    {/*
+    {
         public OracleConnection conn;
         string pcn = "dbi292195";
         string pw = "kd1qoIM98M";
@@ -18,6 +19,6 @@ namespace ThijnVanDijk_IndividueleOpdrach_SE22
             conn = new OracleConnection();
             conn.ConnectionString = "User Id=" + pcn + ";Password=" + pw + ";Data Source=" + " //192.168.15.50:1521/fhictora" + ";";
         }
-      */
+      
     }
 }
--- ThijnVanDijk_IndividueleOpdrach_SE22/ThijnVanDijk_IndividueleOpdrach_SE22/Site.Master.designer.cs
+++ ThijnVanDijk_IndividueleOpdrach_SE22/ThijnVanDijk_IndividueleOpdrach_SE22/Site.Master.designer.cs
@@ -22,13 +22,22 @@ namespace ThijnVanDijk_IndividueleOpdrach_SE22 {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
-        /// Image1 control.
+        /// HyperLink2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
+        /// Image2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
         
         /// <summary>
         /// HyperLink1 control.
